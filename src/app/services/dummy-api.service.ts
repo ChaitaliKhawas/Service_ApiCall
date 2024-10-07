b4f2054de96b88a9f1c93271e9622829b7bf091e
ChaitaliKhawas <chaitalikhawas@gmail.com>
2024-10-07 18:01:40 +0530

    first commit

---
+++ src/app/services/dummy-api.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DummyApiService {
+
+  constructor(private http:HttpClient) { }
+
+  getQuote(): Observable <any>{
+    // alert('in service')
+    // call api
+    return this.http.get('https://dummyjson.com/quotes/1')
+  }
+    getAllPosts(): Observable <any>{
+      return this.http.get('https://jsonplaceholder.typicode.com/posts');
+    }
+  }
+
